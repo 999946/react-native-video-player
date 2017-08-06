e223de0d4a2809c02f53657f2122582ceba45d0d
lzw <mingxin2014@gmail.com>
2017-08-06 15:46:59 +0800

    rn 0.47

--- android/src/main/java/com/reactlibrary/video/player/RNVideoPlayerPackage.java
+++ android/src/main/java/com/reactlibrary/video/player/RNVideoPlayerPackage.java
@@ -16,7 +16,6 @@ public class RNVideoPlayerPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNVideoPlayerModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
